dbd5453ee4bb63832373acfad85c2e0bf39c3284
GRANT HOECHST <granthoechst@college.harvard.edu>
2017-10-16 21:52:58 -0400

    added the project

---
+++ Prisoners/Assets/IgnoreCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player1" ||
+            collision.gameObject.tag == "Player2" ||
+            collision.gameObject.tag == "Chain")
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
+    }
+}
---
+++ Prisoners/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float topSpeed = 10f;
+    bool facingRight = true;
+
+    private void FixedUpdate()
+    {
+        float move;
+        if (gameObject.tag == "Player1")
+            move = Input.GetAxis("Horizontal2");
+        else
+            move = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        //if (move > 0 && !facingRight)
+        //    Flip();
+        //else if (move < 0 && facingRight)
+        //    Flip();
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+
+        theScale.x *= -1;
+
+        transform.localScale = theScale;
+    }
+
+}
