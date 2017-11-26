0a68a43376cd102c09db3bf9d3b61b1dd800a50f
Chris Rodowicz <crodowicz@college.harvard.edu>
2017-11-26 17:42:42 -0500

    short demo sequence

---
+++ Prisoners/Assets/ResetPos.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPos : MonoBehaviour {
+
+    // Update is called once per frame
+    Transform startPos;
+
+	void Update () {
+		if(Input.GetButtonDown("ResetPos"))
+        {
+            transform.position = new Vector3(-32f, 7f, 0f);
+        }
+	}
+}
