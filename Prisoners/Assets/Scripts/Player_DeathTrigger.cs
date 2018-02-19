6e6a18cef8ca4ec07ed248e640770b371673ee0f
Chris Rodowicz <crodowicz@college.harvard.edu>
2018-02-18 23:13:00 -0500

    Player enters deathzone trigger and that reloads current level

---
+++ Prisoners/Assets/Scripts/Player_DeathTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_DeathTrigger : MonoBehaviour {
+
+    private string tag_deathTrigger = "Death";
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == tag_deathTrigger)
+        {
+            Debug.Log("You died.");
+            ReloadLevel();
+        }
+    }
+
+    void ReloadLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+    }
+}
