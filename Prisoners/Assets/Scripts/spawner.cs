911597904d0279d5b91776247deb3f2e6a3c6a9c
kendallzhu <kzhu@college.harvard.edu>
2018-03-31 13:23:08 -0400

    prototype spawner added

---
+++ Prisoners/Assets/Scripts/spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+    public GameObject item;                // The enemy prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        // If the spawner is off
+        if (false)
+        {
+            return;
+        }
+        // Create an instance of the item prefab right below the spawner
+        Instantiate(item, transform);
+    }
+}
