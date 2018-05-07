c7e3ddec64babdd0ec07dba37d815671da7190e5
GRANT HOECHST <granthoechst@college.harvard.edu>
2018-05-07 16:57:38 -0400

    music and chapel

---
+++ Prisoners/Assets/Scripts/MusicControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MusicControl : MonoBehaviour {
+
+	public AudioMixerSnapshot[] fades;
+
+	public AudioSource stingsource;
+	public AudioClip stinger;
+
+	public float transition = 0.4f;
+
+	void Start()
+	{
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+	{
+		fades [scene.buildIndex].TransitionTo (transition);
+		stingsource.clip = stinger;
+		stingsource.Play ();
+	}
+}
