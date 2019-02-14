32be22d9fdb93f49093405845b6fb15016e7a569
kendallzhu <kzhu@college.harvard.edu>
2019-02-14 16:15:50 -0500

    updated to new version of unity (tmpro broke), made levels more playable and added hints, fixed annoying controls

--- Prisoners/Assets/Scripts/Lift.cs
+++ Prisoners/Assets/Scripts/Lift.cs
@@ -17,34 +17,41 @@ public class Lift : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        ceilingCheck = Physics2D.Raycast(transform.position + new Vector3(.1f, .2f, 0), Vector2.up, distance, 1 << LayerMask.NameToLayer("World"));
+        GameObject player2 = GameObject.FindGameObjectWithTag("Player2");
+        Vector3 pos = transform.position;
+        ceilingCheck = Physics2D.Raycast(pos + new Vector3(.1f, .2f, 0), Vector2.up, distance, 1 << LayerMask.NameToLayer("World"));
         if (Input.GetButton ("GrabBig")) {   
             if (!lifting && !ceilingCheck)
             {
                 Physics2D.queriesStartInColliders = false;
-				hit = Physics2D.Raycast (transform.position + new Vector3(-.5f,0,0), Vector2.right, distance, 1 << LayerMask.NameToLayer("Default"));
-				if (hit.collider != null && hit.collider.gameObject.tag == "Player2") {
+                /* hit = Physics2D.Linecast (pos + new Vector3(-.5f, 0.4f, 0), pos + new Vector3(1.5f, 0.4f, 0), 1 << LayerMask.NameToLayer("Default"));
+                Debug.Log(hit.collider);
+                if (hit.collider != null && hit.collider.gameObject.tag == "Player2") {
 					lifting = true;
-				}
+				}*/
+                if (player2 && Vector3.Distance(pos + new Vector3(.5f, 0, 0), player2.transform.position) < 1.2f)
+                {
+                    lifting = true;
+                }
 			}
 		}
 		if (lifting) {
-			hit.collider.attachedRigidbody.position = holdPoint.position;
-			hit.collider.gameObject.GetComponent<DistanceJoint2D>().enableCollision = true;
+			player2.GetComponent<Rigidbody2D>().position = holdPoint.position;
+			player2.GetComponent<DistanceJoint2D>().enableCollision = true;
             animator.SetBool("Lifting", true);
         }
 		if (lifting && (Input.GetButton ("Up2") || ceilingCheck))
 		{
 			lifting = false;
-			hit.collider.gameObject.GetComponent<DistanceJoint2D> ().enableCollision = false;
-			hit.collider.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x,30);
+			player2.GetComponent<DistanceJoint2D> ().enableCollision = false;
+			player2.GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x,30);
             animator.SetBool("Lifting", false);
         }
 	}
 
 	void OnDrawGizmos(){
-		Gizmos.color = Color.green;
-
-		Gizmos.DrawLine (transform.position + new Vector3(0,1,0), transform.position + new Vector3(0,1,0) + Vector3.right * distance);
-	}
+        Vector3 pos = transform.position;
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(pos + new Vector3(-.5f, 0.4f, 0), pos + new Vector3(1.5f, 0.4f, 0));
+    }
 }
